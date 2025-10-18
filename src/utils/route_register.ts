247db8e3df3103fa3915d144364d8947144b9793
Nacxht <zombiecrafter069@gmail.com>
2025-10-18 21:52:53 +0700

    style: change variable name

--- src/utils/route_register.ts
+++ src/utils/route_register.ts
@@ -1,9 +1,9 @@
 import type { Hono } from "hono";
 import type { App } from "../main.ts";
 
-import { auth } from "../routes/auth_route.ts";
-import { todo } from "../routes/todo_route.ts";
-import { user } from "../routes/user_route.ts";
+import { authRoute } from "../routes/auth_route.ts";
+import { todoRoute } from "../routes/todo_route.ts";
+import { userRoute } from "../routes/user_route.ts";
 
 const routes: Array<Hono> = [
   auth,
