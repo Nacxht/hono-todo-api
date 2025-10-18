286deca1cf4b356ff4ba22d22c4dc68d8f0d9225
Nacxht <zombiecrafter069@gmail.com>
2025-10-18 21:51:45 +0700

    style: change variable name

--- src/routes/auth_route.ts
+++ src/routes/auth_route.ts
@@ -1,18 +1,18 @@
 import { Hono } from "hono";
 
-export const auth = new Hono().basePath("/auth");
+export const authRoute = new Hono().basePath("/auth");
 
 // registering a new user
-auth.post("/register", (c) => {
+authRoute.post("/register", (c) => {
   return c.text("Registering a new user");
 });
 
 // login
-auth.post("/login", (c) => {
+authRoute.post("/login", (c) => {
   return c.text("Login endpoint");
 });
 
 // logout
-auth.delete("/logout", (c) => {
+authRoute.delete("/logout", (c) => {
   return c.text("Logout endpoint");
 });
