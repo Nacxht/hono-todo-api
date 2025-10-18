dd1ba5ababa856df400aa12f6199854271e9ff36
Nacxht <zombiecrafter069@gmail.com>
2025-10-18 16:40:36 +0700

    feat: added base notFound and onError handlers

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,18 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.notFound((c) => {
+  return c.json({
+    status: "error",
+    message: "Error 404 - Page not Found",
+  }, 404);
+});
+
+app.notFound((c) => {
+  return c.json({
+    status: "error",
+    message: "An Error message",
+  });
+});
+
 Deno.serve(app.fetch);
